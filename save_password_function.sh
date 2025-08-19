dc081e68a5965c12e221e354fd3363f621aff55a
t.nakamura <~~~@users.noreply.github.com>
2025-08-19 16:50:46 +0900

    feat: #6 パスワード保存関数を作成する

---
+++ save_password_function.sh
@@ -0,0 +1,12 @@
+#!bin/bash
+
+save_password() {
+  read -p "サービス名を入力してください：" service_name
+  read -p "ユーザー名を入力してください：" user_name
+  read -s -p "パスワードを入力してください：" password
+
+  # 次の行がターミナル上で前の行にインライン表示されることを防ぐために改行する
+  echo
+  echo "${service_name}:${user_name}:${password}" >> storage.txt
+  echo "パスワードの追加は成功しました。"
+}
