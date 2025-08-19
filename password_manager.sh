d4ca59c4b14d32e3a18779b2e23e993a000e0b09
t.nakamura <~~~@users.noreply.github.com>
2025-08-19 14:23:33 +0900

    feat: #1 入力情報をファイルに保存する

--- password_manager.sh
+++ password_manager.sh
@@ -6,4 +6,5 @@ read -p "ユーザー名を入力してください：" user_name
 read -s -p "パスワードを入力してください：" password
 # 次の行の"Thank you!"が前の行にインライン表示されるので改行する
 echo
+echo "${service_name}:${user_name}:${password}" >> storage.txt
 echo "Thank you!"
